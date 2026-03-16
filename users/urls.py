2c2b828420efca6532068bbaceb3fe743d7a2d68
Munyeshya <obedbenny54@gmail.com>
2026-03-16 16:54:41 +0200

    Add authenticated me endpoint for current user

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", ProfileView.as_view(), name="profile"),
+    path("me/", ProfileView.as_view(), name="me"),
 ]
