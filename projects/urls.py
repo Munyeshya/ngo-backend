27ae9fa008cc776736954795faa133ad38eea8c1
Munyeshya <obedbenny54@gmail.com>
2026-03-17 01:59:44 +0200

    Add project and partner detail update and delete API endpoints

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PartnerListCreateView, ProjectListCreateView
+from .views import *
 
 urlpatterns = [
     path("partners/", PartnerListCreateView.as_view(), name="partner-list-create"),
+    path("partners/<int:pk>/", PartnerDetailView.as_view(), name="partner-detail"),
     path("", ProjectListCreateView.as_view(), name="project-list-create"),
+    path("<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -33,6 +33,42 @@ class PartnerListCreateView(generics.ListCreateAPIView):
         )
 
 
+class PartnerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Partner.objects.all()
+    serializer_class = PartnerSerializer
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [IsAdminUserRole()]
+        return [permissions.IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        return success_response(
+            message="Partner fetched successfully.",
+            data=serializer.data,
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return success_response(
+            message="Partner updated successfully.",
+            data=serializer.data,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return success_response(
+            message="Partner deleted successfully.",
+            data={},
+        )
+
+
 class ProjectListCreateView(generics.ListCreateAPIView):
     queryset = Project.objects.select_related("created_by").prefetch_related("partners")
     serializer_class = ProjectSerializer
@@ -61,4 +97,43 @@ class ProjectListCreateView(generics.ListCreateAPIView):
             message="Project created successfully.",
             data=serializer.data,
             status_code=status.HTTP_201_CREATED,
+        )
+
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.select_related("created_by").prefetch_related("partners")
+    serializer_class = ProjectSerializer
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [IsAdminUserRole()]
+        return [permissions.IsAuthenticated()]
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object())
+        return success_response(
+            message="Project fetched successfully.",
+            data=serializer.data,
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return success_response(
+            message="Project updated successfully.",
+            data=serializer.data,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return success_response(
+            message="Project deleted successfully.",
+            data={},
         )
