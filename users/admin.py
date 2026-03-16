cbff345d3377ffefa952760c6679654fb260ee08
Munyeshya <obedbenny54@gmail.com>
2026-03-16 16:23:21 +0200

    Recreate virtual environment with Python 3.12 for Django compatibility

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("id", "username", "email", "role", "is_verified", "is_staff", "is_active")
+    list_filter = ("role", "is_verified", "is_staff", "is_active")
+    search_fields = ("username", "email", "phone_number")
+    ordering = ("id",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("phone_number", "role", "is_verified")}),
+    )
