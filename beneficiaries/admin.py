66e533be6814cd2601753b7c628de8cb6aee3231
Munyeshya <obedbenny54@gmail.com>
2026-03-17 17:18:36 +0200

    Register beneficiary models in Django admin

--- beneficiaries/admin.py
+++ beneficiaries/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Beneficiary, BeneficiaryImage
 
-# Register your models here.
+
+class BeneficiaryImageInline(admin.TabularInline):
+    model = BeneficiaryImage
+    extra = 1
+
+
+@admin.register(Beneficiary)
+class BeneficiaryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "project", "is_active", "created_at")
+    list_filter = ("is_active", "project", "created_at")
+    search_fields = ("name", "description", "project__title")
+    ordering = ("-created_at",)
+    inlines = [BeneficiaryImageInline]
+
+
+@admin.register(BeneficiaryImage)
+class BeneficiaryImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "beneficiary", "caption", "created_at")
+    search_fields = ("beneficiary__name", "caption")
+    ordering = ("-created_at",)
