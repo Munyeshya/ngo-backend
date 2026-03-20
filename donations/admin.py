9d0a91177b67906003f9f74a361455d0933f24a9
Munyeshya <obedbenny54@gmail.com>
2026-03-20 21:13:14 +0200

    Register donation model in Django admin

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "project",
+        "donor_name",
+        "donor_email",
+        "amount",
+        "payment_method",
+        "status",
+        "is_anonymous",
+        "donor",
+        "donated_at",
+    )
+    list_filter = (
+        "status",
+        "payment_method",
+        "is_anonymous",
+        "donated_at",
+        "project",
+    )
+    search_fields = (
+        "donor_name",
+        "donor_email",
+        "transaction_reference",
+        "project__title",
+        "donor__username",
+    )
+    ordering = ("-donated_at",)
