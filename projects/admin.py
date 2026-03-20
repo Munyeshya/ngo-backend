e213b14f2d470d029db19c24de4f08599ecee89b
Munyeshya <obedbenny54@gmail.com>
2026-03-20 21:59:31 +0200

    Register project update models in Django admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Partner
+from .models import Project, Partner, ProjectUpdate, ProjectUpdateImage
 
 
 @admin.register(Partner)
@@ -8,6 +8,27 @@ class PartnerAdmin(admin.ModelAdmin):
     list_filter = ("is_active", "created_at")
     search_fields = ("name", "website")
     ordering = ("name",)
+
+
+class ProjectUpdateImageInline(admin.TabularInline):
+    model = ProjectUpdateImage
+    extra = 1
+
+
+@admin.register(ProjectUpdate)
+class ProjectUpdateAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "project", "created_by", "created_at")
+    list_filter = ("project", "created_at")
+    search_fields = ("title", "description", "project__title")
+    ordering = ("-created_at",)
+    inlines = [ProjectUpdateImageInline]
+
+
+@admin.register(ProjectUpdateImage)
+class ProjectUpdateImageAdmin(admin.ModelAdmin):
+    list_display = ("id", "project_update", "caption", "created_at")
+    search_fields = ("project_update__title", "caption")
+    ordering = ("-created_at",)
 
 
 @admin.register(Project)
