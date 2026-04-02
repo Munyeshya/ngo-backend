3ab46d21c9cfe822299da69706f5522320b48001
Munyeshya <obedbenny54@gmail.com>
2026-04-02 22:26:35 +0200

    Create claimable donor profiles from guest donations

--- donations/serializers.py
+++ donations/serializers.py
@@ -1,5 +1,11 @@
+from uuid import uuid4
+
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 from rest_framework import serializers
 from .models import Donation
+
+User = get_user_model()
 
 
 class DonationSerializer(serializers.ModelSerializer):
@@ -91,6 +97,21 @@ class PublicDonationCreateSerializer(serializers.ModelSerializer):
 
         if request and request.user and request.user.is_authenticated:
             donor_user = request.user
+        else:
+            donor_email = validated_data["donor_email"]
+            donor_user = User.objects.filter(email=donor_email, role=User.ROLE_DONOR).first()
+
+            if donor_user is None and not User.objects.filter(email=donor_email).exists():
+                username_root = slugify(donor_email.split("@")[0]) or "donor"
+                donor_user = User(
+                    username=f"{username_root}-{uuid4().hex[:8]}",
+                    email=donor_email,
+                    role=User.ROLE_DONOR,
+                    is_active=True,
+                    is_verified=False,
+                )
+                donor_user.set_unusable_password()
+                donor_user.save()
 
         donation = Donation.objects.create(
             donor=donor_user,
