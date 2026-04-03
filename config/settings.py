64e2ce70f3c201d3eb4004a5f7588ec776a9e43a
Munyeshya <obedbenny54@gmail.com>
2026-04-03 17:18:17 +0200

    Increase API default page size to twelve

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 12,
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
