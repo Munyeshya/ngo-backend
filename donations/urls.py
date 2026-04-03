29a6046993029fafc71eda5d3db97ae2e936afea
Munyeshya <obedbenny54@gmail.com>
2026-04-03 05:59:34 +0200

    Route donation type analytics endpoint

--- donations/urls.py
+++ donations/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import DonationListCreateView, DonationDetailView, MyDonationsView
+from .views import (
+    DonationListCreateView,
+    DonationDetailView,
+    DonationTypeSupportAnalyticsView,
+    MyDonationsView,
+)
 
 urlpatterns = [
     path("", DonationListCreateView.as_view(), name="donation-list-create"),
+    path("type-support-analytics/", DonationTypeSupportAnalyticsView.as_view(), name="donation-type-support-analytics"),
     path("my/", MyDonationsView.as_view(), name="my-donations"),
     path("<int:pk>/", DonationDetailView.as_view(), name="donation-detail"),
 ]
